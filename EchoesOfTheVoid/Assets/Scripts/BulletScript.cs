7c3f85e63b1e72a398a5bf15f5721fa0d6b71011
thomasisebeck <thomasisebeck@gmail.com>
2023-05-10 21:50:01 +0200

    added bullet prefab and script

--- EchoesOfTheVoid/Assets/Scripts/BulletScript.cs
+++ EchoesOfTheVoid/Assets/Scripts/BulletScript.cs
@@ -7,16 +7,30 @@ public class BulletScript : MonoBehaviour
 {
 
     public float speed;
+    public float innacuracy;
+    private float jitter;
+    public Rigidbody2D rb;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        jitter = Random.Range(-innacuracy, innacuracy);
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(Vector3.right * speed * Time.deltaTime);
+        if (jitter > 0)  
+            jitter += 0.001f;
+        else
+            jitter -= 0.001f;
+
+
+      
+
+        /*
+        transform.Translate((Vector3.up * jitter)  * Time.deltaTime);
+        transform.Translate(Vector3.right * speed * Time.deltaTime);*/
     }
 }
