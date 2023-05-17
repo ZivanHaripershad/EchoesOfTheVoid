4884296028926ba77daee68368a9c6e1060fd358
thomasisebeck <thomasisebeck@gmail.com>
2023-05-17 10:23:00 +0200

    enemies can crash into the planet and explode

---
+++ EchoesOfTheVoid/Assets/Scripts/Enemy/DestroyEnemy.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using UnityEngine;
+
+public class DestroyEnemy : MonoBehaviour
+{
+
+    [SerializeField]
+    GameObject explosion;
+
+    private bool canBeDestroyed = false;
+
+    // Start is called before the first frame update
+    IEnumerator Start()
+    {
+        yield return new WaitForSeconds(0.1f);
+        canBeDestroyed = true;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (canBeDestroyed)
+        {
+            Instantiate(explosion, transform.position, Quaternion.identity);
+            Destroy(gameObject);
+        }
+    }
+}
