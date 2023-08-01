4a2d3607b4316edeeca3518df866c015e7d3063a
Zivan Haripershad <Zivan.Haripershad@standardbank.co.za>
2023-08-01 22:18:47 +0200

    Initial Tutorial Design

--- EchoesOfTheVoid/Assets/Scripts/Enemy/EnemySpawning.cs
+++ EchoesOfTheVoid/Assets/Scripts/Enemy/EnemySpawning.cs
@@ -32,7 +32,7 @@ public class EnemySpawning : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    public void SpawnEnemies()
     {
         timer += Time.deltaTime;
 
