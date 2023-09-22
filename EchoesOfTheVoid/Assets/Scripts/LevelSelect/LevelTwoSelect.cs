938fb6a1a840c7cd850e16e5c628969c744eecd1
thomasisebeck <thomasisebeck@gmail.com>
2023-09-22 10:45:12 +0200

    created level 2 scene

--- EchoesOfTheVoid/Assets/Scripts/LevelSelect/LevelTwoSelect.cs
+++ EchoesOfTheVoid/Assets/Scripts/LevelSelect/LevelTwoSelect.cs
@@ -1,9 +1,11 @@
 
+using UnityEngine.SceneManagement;
+
 public class LevelTwoSelect : LevelSelectButtonInterface
 {
     public override void OnMouseDown()
     {
-        AudioManager.Instance.PlaySFX("CannotDeposit");
-        // SceneManager.LoadScene("Level2");
+        AudioManager.Instance.PlaySFX("ButtonClicked");
+        SceneManager.LoadScene("Level2");
     }
 }
