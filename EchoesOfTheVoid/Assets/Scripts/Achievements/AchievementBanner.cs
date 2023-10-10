44eb335249c26abf80c4c24ec16b9d5ad441104c
Zivan Haripershad <Zivan.Haripershad@standardbank.co.za>
2023-10-10 22:28:53 +0200

    Added achievement banners

---
+++ EchoesOfTheVoid/Assets/Scripts/Achievements/AchievementBanner.cs
@@ -0,0 +1,51 @@
+﻿
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AchievementBanner: MonoBehaviour
+{
+    private float achievementBannerWaitTime;
+    private Text missionObjectiveText;
+    [SerializeField] private GameObject achievementObjectiveCanvas;
+
+    private void Start()
+    {
+        missionObjectiveText = achievementObjectiveCanvas.transform.Find("Notification").GetComponent<Text>();
+        AchievementsManager.Instance.CheckIfCollectorCompleted();
+        AchievementsManager.Instance.CheckIfDeflectorCompleted();
+        AchievementsManager.Instance.CheckIfGodModeCompleted();
+        AchievementsManager.Instance.CheckIfMillionaireCompleted();
+        AchievementsManager.Instance.CheckIfProtectorCompleted();
+        AchievementsManager.Instance.CheckIfRampageCompleted();
+        AchievementsManager.Instance.CheckIfRiskTakerCompleted();
+        AchievementsManager.Instance.CheckIfScholarCompleted();
+        AchievementsManager.Instance.CheckIfSpeedRunnerCompleted();
+    }
+
+    private void Update()
+    {
+        Queue<string> achievementUpdates = AchievementsManager.Instance.GetAchievementUpdates();
+        bool isBannerAvailable = AchievementsManager.Instance.GetIsBannerAvailable();
+
+        if (achievementUpdates.Count > 0 && isBannerAvailable)
+        {
+            achievementBannerWaitTime = AchievementsManager.Instance.GetBannerWaitTime();
+            AchievementsManager.Instance.SetIsBannerAvailable(false);
+            achievementObjectiveCanvas.SetActive(true);
+            var missionUpdate = achievementUpdates.Dequeue();
+            AudioManager.Instance.PlaySFX("ObjectiveInProgress");
+            missionObjectiveText.text = missionUpdate;
+        }
+        
+        if (achievementBannerWaitTime <= 0)
+        {
+            AchievementsManager.Instance.SetIsBannerAvailable(true);
+            achievementObjectiveCanvas.SetActive(false);
+            AchievementsManager.Instance.ResetBannerWaitTime();
+        }
+        
+        achievementBannerWaitTime -= Time.deltaTime;
+    }
+}
--- EchoesOfTheVoid/Assets/Scripts/SettingsMenu/AudioManager.cs
+++ EchoesOfTheVoid/Assets/Scripts/SettingsMenu/AudioManager.cs
@@ -40,7 +40,7 @@ public class AudioManager : MonoBehaviour
 
         currentlyPlaying = MusicFileNames.NoMusic;
         currentMusicSliderValue = 0.118f;
-        currentSfxSliderValue = 0.15f;
+        currentSfxSliderValue = 0.118f;
     }
 
     public enum MusicFileNames
