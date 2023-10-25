ea5b3e222245307e0c7b048da8331e0fb28ad88b
thomasisebeck <thomasisebeck@gmail.com>
2023-10-25 15:14:32 +0200

    More readable L3 spawning

--- EchoesOfTheVoid/Assets/Scripts/Enemy/EnemySpawningLevel3.cs
+++ EchoesOfTheVoid/Assets/Scripts/Enemy/EnemySpawningLevel3.cs
@@ -50,56 +50,62 @@ public class EnemySpawningLevel3 : EnemySpawning {
             for (int i = 0; i < enemiesToSpawn; i++)
             {
                 int result = Random.Range(0, 12);
+                float randomNumber;
 
-                if (result == 1 || result == 2 || result == 3)
+                switch (result)
                 {
-                    //shield enemies
-                    float randomNumber = Random.Range(0f, gameManagerData.spawnTimerVariation);
-                    yield return new WaitForSeconds(gameManagerData.spawnInterval + randomNumber);
-            
-                    //get a random spawn point
-                    int point = Random.Range(0, shieldEnemySpawnPoints.Length - 1);
-                    Instantiate(shieldEnemy, new Vector3(
-                            shieldEnemySpawnPoints[point].transform.position.x,
-                            shieldEnemySpawnPoints[point].transform.position.y, 0),
-                        quaternion.identity);
-                }
-                else if(result == 4)
-                {
-                    float randomNumber = Random.Range(0f, gameManagerData.spawnTimerVariation);
-                    yield return new WaitForSeconds(gameManagerData.spawnInterval + randomNumber);
-                    Instantiate(zigZagEnemy, zigzagEnemySpawnPoints[currentZigZagSpawnPoint++], Quaternion.identity);
-                    if (currentZigZagSpawnPoint > zigzagEnemySpawnPoints.Length - 1)
-                        currentZigZagSpawnPoint = 0;
-                }
-                else if (result == 5 || result == 6)
-                {
-                    //Minion enemy
-                    int corner = Random.Range(0, 4);
-
-                    switch (corner)
-                    {
-                        case 0: //top
-                            Instantiate(minionEnemy, new Vector3(0, 5.87f, 0), Quaternion.identity);
-                            break;
-                        case 1: //left
-                            Instantiate(minionEnemy, new Vector3(-10.78f, 0, 0), Quaternion.identity);
-                            break;
-                        case 2: //bottom
-                            Instantiate(minionEnemy, new Vector3(0, -5.87f, 0), Quaternion.identity);
-                            break;
-                        case 3: //right
-                            Instantiate(minionEnemy, new Vector3(10.78f, 0, 0), Quaternion.identity);
-                            break;
-                    }
+                    case 1: case 2: case 3: //shield enemies
+                        
+                        randomNumber = Random.Range(0f, gameManagerData.spawnTimerVariation);
+                        yield return new WaitForSeconds(gameManagerData.spawnInterval + randomNumber);
+                
+                        //get a random spawn point
+                        int point = Random.Range(0, shieldEnemySpawnPoints.Length - 1);
+                        Instantiate(shieldEnemy, new Vector3(
+                                shieldEnemySpawnPoints[point].transform.position.x,
+                                shieldEnemySpawnPoints[point].transform.position.y, 0),
+                            quaternion.identity);
+                        break;
                     
-                }
-                else
-                {
-                    //purple enemies in a wave
-                    float randomNumber = Random.Range(0f, gameManagerData.spawnTimerVariation);
-                    yield return new WaitForSeconds(gameManagerData.spawnInterval + randomNumber);
-                    Instantiate(pathFollowingEnemy, Vector3.zero, quaternion.identity);
+                    case 4: //zigzag 
+                        
+                         randomNumber = Random.Range(0f, gameManagerData.spawnTimerVariation);
+                        yield return new WaitForSeconds(gameManagerData.spawnInterval + randomNumber);
+                        Instantiate(zigZagEnemy, zigzagEnemySpawnPoints[currentZigZagSpawnPoint++], Quaternion.identity);
+                        if (currentZigZagSpawnPoint > zigzagEnemySpawnPoints.Length - 1)
+                            currentZigZagSpawnPoint = 0;
+                         break;
+                    
+                    case 5:  case 6: //minion
+                        
+                        if (GameObject.FindGameObjectWithTag("MineEnemy"))
+                        {
+                            //Minion enemy
+                            int corner = Random.Range(0, 4);
+    
+                            switch (corner)
+                            {
+                                case 0: //top
+                                    Instantiate(minionEnemy, new Vector3(0, 5.87f, 0), Quaternion.identity);
+                                    break;
+                                case 1: //left
+                                    Instantiate(minionEnemy, new Vector3(-10.78f, 0, 0), Quaternion.identity);
+                                    break;
+                                case 2: //bottom
+                                    Instantiate(minionEnemy, new Vector3(0, -5.87f, 0), Quaternion.identity);
+                                    break;
+                                case 3: //right
+                                    Instantiate(minionEnemy, new Vector3(10.78f, 0, 0), Quaternion.identity);
+                                    break;
+                            }
+                        }
+                        break;
+                    
+                    default: //purple 
+                        randomNumber = Random.Range(0f, gameManagerData.spawnTimerVariation);
+                        yield return new WaitForSeconds(gameManagerData.spawnInterval + randomNumber);
+                        Instantiate(pathFollowingEnemy, Vector3.zero, quaternion.identity);
+                        break;
                 }
               
             }
