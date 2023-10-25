ed79b47aebcadb5ab955690fee292077ba367ef7
thomasisebeck <thomasisebeck@gmail.com>
2023-10-25 14:08:38 +0200

    minion shield logic added

--- EchoesOfTheVoid/Assets/Scripts/Enemy/MineDropping/Shield.cs
+++ EchoesOfTheVoid/Assets/Scripts/Enemy/MineDropping/Shield.cs
@@ -8,11 +8,16 @@ public class Shield : MonoBehaviour
     private GameObject mineEnemy;
 
     [SerializeField] private float followSpeed;
+    [SerializeField] private float jitterAdditionSpeed;
+    [SerializeField] private float randomJitter;
+
+    private float currentJitterX; 
+    private float currentJitterY; 
 
     // Start is called before the first frame update
     void Start()
     {
-        mineEnemy = GameObject.FindGameObjectWithTag("MineEnemy");
+        mineEnemy = GameObject.FindGameObjectWithTag("MineEnemyShieldCenter");
     }
 
     // Update is called once per frame
@@ -21,7 +26,24 @@ public class Shield : MonoBehaviour
         if (mineEnemy == null)
             Destroy(gameObject);
 
-        transform.position = Vector3.Lerp(transform.position, mineEnemy.transform.position, followSpeed);
+        float jitterX = Random.Range(-randomJitter, randomJitter);
+        float jitterY = Random.Range(-randomJitter, randomJitter);
+
+        currentJitterX += jitterX;
+        currentJitterY += jitterY;
+
+        if (currentJitterX > randomJitter)
+            currentJitterX = randomJitter;
+        if (currentJitterY > randomJitter)
+            currentJitterY = randomJitter;
+        
+        if (currentJitterX < -randomJitter)
+            currentJitterX = -randomJitter;
+        if (currentJitterY < -randomJitter)
+            currentJitterY = -randomJitter;
+        
+        transform.position = Vector3.Lerp(transform.position, new Vector3(mineEnemy.transform.position.x, mineEnemy.transform.position.y, 0), followSpeed * Time.deltaTime);
+        transform.position = Vector3.Lerp(transform.position, new Vector3(mineEnemy.transform.position.x + jitterX, mineEnemy.transform.position.y + jitterY, 0), jitterAdditionSpeed * Time.deltaTime);
 
     }
 }
--- EchoesOfTheVoid/Assets/Scripts/Enemy/MinionEnemy/FollowGreenBoss.cs
+++ EchoesOfTheVoid/Assets/Scripts/Enemy/MinionEnemy/FollowGreenBoss.cs
@@ -8,10 +8,17 @@ public class FollowGreenBoss : MonoBehaviour
     [SerializeField] private GameObject explosion;
     private GameObject greenBoss;
     [SerializeField] private float smoothSpeed;
+    [SerializeField] private float followXOffset;
+    [SerializeField] private float followYOffset;
+    [SerializeField] private float shieldDepositTime;
+    [SerializeField] private GameObject minionShield;
+
+    private float cooldown;
 
     void Start()
     {
         greenBoss = GameObject.FindGameObjectWithTag("MineEnemy");
+        cooldown = 0; 
     }
 
     // Update is called once per frame
@@ -23,11 +30,10 @@ public class FollowGreenBoss : MonoBehaviour
         if (greenBoss != null)
         {
             Vector3 greenPosition = greenBoss.transform.position;
-            Vector3 targetPosition = new Vector3(greenPosition.x, greenPosition.y, 0);
             Vector3 myPosition = gameObject.transform.position;
-    
+        
             // Use Lerp to smoothly move towards the target position
-            transform.position = Vector3.Lerp(new Vector3(myPosition.x, myPosition.y + 10.5f, myPosition.z), targetPosition, smoothSpeed * Time.deltaTime);
+            transform.position = Vector3.Lerp(myPosition, new Vector3(greenPosition.x + followXOffset, greenPosition.y + followXOffset, greenPosition.z), smoothSpeed * Time.deltaTime);
         }
         else //if not found, self-destruct
         {
